78b4295de791958d84e43d347cfb9aa2e3242615
Zander Zhuang <zander.zhuang@gmail.com>
2020-09-13 01:12:25 +1000

    basic single room chat working

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask, render_template, url_for
+import os
+from flask import Flask, render_template, url_for, redirect, request
+from flask_socketio import SocketIO
+
+from forms import UserForm, ChatBox
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'this-is-a-secret-key-for-this'
+socketio = SocketIO(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    form = UserForm()
+    if form.validate_on_submit():
+        return redirect(url_for("lobby", user=form.name.data))
+    return render_template("index.html", form=form)
+
+def messageReceived(methods=["GET", "POST"]):
+    print('message was received')
+
+@socketio.on('my event')
+def handle_receive_message(json, methods=["GET", "POST"]):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+@app.route("/lobby/", defaults={'room' : 'main', 'user' : 'Anon'}, methods=["GET", "POST"])
+@app.route("/lobby/<room>/<user>", methods=["GET", "POST"])
+def lobby(room, user):
+    print(user)
+    form = ChatBox()
+    if form.validate_on_submit():
+        return redirect(url_for("lobby", user=user))
+
+    return render_template("lobby.html", form=form, user=user)
+
+if __name__ == 'main':
+    socketio.run(app, debug=True)
---
+++ forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+class UserForm(FlaskForm):
+    name = StringField('Nickname', validators=[DataRequired()])
+    room = StringField('Room', validators=[DataRequired()])
+    create = SubmitField('Create')
+    join = SubmitField('Join')
+
+class ChatBox(FlaskForm):
+    msg = TextAreaField('Message')
+    send = SubmitField('Send')
